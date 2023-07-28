32707485087a20402bbd188460f54a70ba47b24c
Juan Pedro Bastias <juanpedrobastiastorresi@gmail.com>
2023-07-28 12:16:28 -0300

    success

--- src/app/models/game.model.ts
+++ src/app/models/game.model.ts
@@ -2,6 +2,13 @@
 
 export interface Game {
   id: number;
+  developer: string;
+  description: string;
+  freetogame_profile_url: string;
+  game_url: string;
+  minimum_system_requirements: string;
+  release_date: Date;
+  status: string;
   title: string;
   thumbnail: string;
   short_description: string;
